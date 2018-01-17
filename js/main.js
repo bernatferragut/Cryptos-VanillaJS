95b9e14330dc1110a98856538f485552eb2971ad
elbernat <elbernat@gmail.com>
2018-01-17 17:50:10 -0500

    Warm up
    Testing Code, warming motors up

--- js/main.js
+++ js/main.js
@@ -2,7 +2,37 @@
 console.log("THIS IS A TEST!");
 
 // selections
-let myList =  document.getElementById("myList");
-for (i=0; i<myList.length; i++) {
-    console.log("element" + i);
+let myTitle = document.getElementById("mainTitle");
+let myButton = document.getElementById("button");
+let myList = document.getElementById("myList");
+let myListItems = document.getElementById("myList").getElementsByTagName("li");
+let newItemCounter = myListItems.length;
+
+// Changing All Collection Items Texts
+// for(let i=0; i<myListItems.length; i++){
+//     myListItems[i].innerHTML = "CRYPTO " + (i+1);
+//     myListItems[i].addEventListener('click', activateItem);
+// }
+
+myList.addEventListener("click", activateItem);
+
+// Adding Items
+myButton.addEventListener('click', addListItem);
+
+// Functions
+function activateItem(e) {
+    if(e.target.nodeName == "LI") {
+        myTitle.innerHTML = e.target.innerHTML;
+        for(let i=0; i<e.target.parentNode.children.length; i++){
+            e.target.parentNode.children[i].classList.remove("red");
+        }
+        e.target.classList.add("red");
+    }
 }
+
+function addListItem() {
+    newItemCounter ++;
+    myList.innerHTML += "<li>Crypto " + newItemCounter + "</li>" 
+}
+
+
