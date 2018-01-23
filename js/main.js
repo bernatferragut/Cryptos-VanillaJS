f25aa6b8e3a0c39285a3acc350fe10aabc126854
elbernat <elbernat@gmail.com>
2018-01-23 10:01:14 -0500

    Cryptos Wallet v1.0
    Crypto Wallet v.1.0 functionning successfully

--- js/main.js
+++ js/main.js
@@ -9,45 +9,114 @@ myButtton.addEventListener('click', getCryptos);
 // GET API DATA
 // 1. AJAX + JSON => Cryptocompare API
 // Cryptocompare/Price/Multi (returns Object + Multicoin object prices)
-let cryptosUrl = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,XLM,EOS,IOT,BTG,XRB,BTS,BAT&tsyms=USD";
+let cryptosUrl = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,LTC,XLM,EOS,TRX,IOT,BTG,XRB,BTS,BAT,XRB&tsyms=USD";
 
 function getCryptos() {
     let myRequest = new XMLHttpRequest();
     myRequest.open('GET',cryptosUrl);
     myRequest.onload = function() {
         let myResponse = JSON.parse(myRequest.responseText);
-        console.log(myResponse);
-        renderHTML(myResponse);
+        console.log(myResponse );
+        console.log(hodl);
+        renderHTML(myResponse, hodl);
     }
     myRequest.send();
 }
 
+// THIS IS MY OWN OBJECT
+let hodl = {
+    BAT : {amount: 6474},
+    BTC : {amount: 4.01},
+    BTG : {amount: 4.01},
+    BTS : {amount: 12500},
+    EOS : {amount: 5860},
+    IOT : {amount: 10200},
+    LTC : {amount: 20.01},
+    TRX : {amount: 6740},
+    XLM : {amount: 1260},
+    XRB : {amount: 100},
+}
 
-function renderHTML(data) {
-    // let myCryptoList = ['BTC', 'EOS', 'IOT'];
-    // for (let i=0; i<myCryptoList.length; i++) {
-    //     let htmlString = '<li> 1 ' + data.myCryptoList[i].USD + ' USD</li>';
-    //     myContainer.insertAdjacentHTML('beforeend', htmlString);
-    // }
-
-    let htmlString = '<li> 1 BTC = ' + data.BTC.USD + ' USD</li>';
+function renderHTML(data, hodl) {
+    // BTC
+    let htmlString =
+    `
+        <table class="u-full-width">
+        <thead>
+            <tr>
+                <th>CRYPTO</th>
+                <th>VALUE</th>
+                <th>HODL</th>
+                <th>USD</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>BAT</td>
+                <td>${data.BAT.USD}</td>
+                <td>${hodl.BAT.amount}</td>
+                <td>${Math.round(hodl.BAT.amount * data.BAT.USD)}</td>
+            </tr>
+                <td>BTC</td>
+                <td>${data.BTC.USD}</td>
+                <td>${hodl.BTC.amount}</td>
+                <td>${Math.round(hodl.BTC.amount * data.BTC.USD)}</td>
+            </tr>
+                <td>BTG</td>
+                <td>${data.BTG.USD}</td>
+                <td>${hodl.BTG.amount}</td>
+                <td>${Math.round(hodl.BTG.amount * data.BTG.USD)}</td>
+            </tr>
+                <td>BTS</td>
+                <td>${data.BTS.USD}</td>
+                <td>${hodl.BTS.amount}</td>
+                <td>${Math.round(hodl.BTS.amount * data.BTS.USD)}</td>
+            </tr>
+                <td>EOS</td>
+                <td>${data.EOS.USD}</td>
+                <td>${hodl.EOS.amount}</td>
+                <td>${Math.round(hodl.EOS.amount * data.EOS.USD)}</td>
+            </tr>
+                <td>IOT</td>
+                <td>${data.IOT.USD}</td>
+                <td>${hodl.IOT.amount}</td>
+                <td>${Math.round(hodl.IOT.amount * data.IOT.USD)}</td>
+            </tr>
+                <td>LTC</td>
+                <td>${data.LTC.USD}</td>
+                <td>${hodl.LTC.amount}</td>
+                <td>${Math.round(hodl.LTC.amount * data.LTC.USD)}</td>
+            </tr>
+                <td>TRX</td>
+                <td>${data.TRX.USD}</td>
+                <td>${hodl.TRX.amount}</td>
+                <td>${Math.round(hodl.TRX.amount * data.TRX.USD)}</td>
+            </tr>
+                <td>XRB</td>
+                <td>${data.XRB.USD}</td>
+                <td>${hodl.XRB.amount}</td>
+                <td>${Math.round(hodl.XRB.amount * data.XRB.USD)}</td>
+            </tr>
+                <td>XLM</td>
+                <td>${data.XLM.USD}</td>
+                <td>${hodl.XLM.amount}</td>
+                <td>${Math.round(hodl.XLM.amount * data.XLM.USD)}</td>
+             </tr>
+        </tbody>
+        <thead>
+        <tr>
+            <th>TOTAL</th>
+            <th>----</th>
+            <th>----</th>
+            <th>${Math.round(hodl.BAT.amount * data.BAT.USD)+Math.round(hodl.BTC.amount * data.BTC.USD)+Math.round(hodl.BTG.amount * data.BTG.USD)+Math.round(hodl.BTS.amount * data.BTS.USD)+Math.round(hodl.EOS.amount * data.EOS.USD)+Math.round(hodl.IOT.amount * data.IOT.USD)+Math.round(hodl.LTC.amount * data.LTC.USD)+Math.round(hodl.TRX.amount * data.TRX.USD)+Math.round(hodl.XLM.amount * data.XLM.USD)+Math.round(hodl.XRB.amount * data.XRB.USD)}</th>
+        </tr>
+    </thead>
+        </table>
+    `
     myContainer.insertAdjacentHTML('beforeend', htmlString);
-    let htmlString2 = '<li> 1 EOS = ' + data.EOS.USD + ' USD</li>';
-    myContainer.insertAdjacentHTML('beforeend', htmlString2);
-    let htmlString3 = '<li> 1 IOT = ' + data.IOT.USD + ' USD</li>';
-    myContainer.insertAdjacentHTML('beforeend', htmlString3);
 
 }
 
 // GET API DATA
 // 1. Fetch + JSON + ES6 Literals=> Cryptocompare API
 // Cryptocompare/Price/Multi (returns Object + Multicoin object prices)
-
-
-
-
-
-
-
-
-
